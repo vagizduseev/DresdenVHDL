9f4433f631f60c96a962b1c58bdbe9f90d2f99f0
vagizduseev <vagiy.d@gmail.com>
2014-07-04 10:25:17 +0200

    Lab commit

--- Labs/Lab4/trigger.vhd
+++ Labs/Lab4/trigger.vhd
@@ -22,19 +22,19 @@ begin
   begin                                 -- induce a state transition.
     if rising_edge(clk) then     -- Positive clock edge.
       -- Synchronous reset
-      if rst = ?1? then                 
-        --eventCounter <= ?0?;
+      if rst = '1' then                 
+        eventCounter <= 0;
       end if;
---      -- Event counter
---      if evt = '1' then
---        eventCounter <= eventCounter + 1;
---        -- Assert the trg signal whenever THRESHOLD is reached
---        if eventCounter = THRESHOLD then
---          trg <= '1';
---        else
---          trg <= '0';
---        end if; -- end Trg assertion          
---      end if; --end Event counter
+      -- Event counter
+      if evt = '1' then
+        eventCounter <= eventCounter + 1;
+        -- Assert the trg signal whenever THRESHOLD is reached
+        if eventCounter = THRESHOLD then
+          trg <= '1';
+        else
+          trg <= '0';
+        end if; -- end Trg assertion          
+      end if; --end Event counter
     end if; -- end Clk event
   end process;
       
