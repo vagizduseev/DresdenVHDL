aa450d2eb02cc3befd91603983b6da3b9de0857a
vagizduseev <vagiz.d@gmail.com>
2014-07-11 16:41:34 +0200

    Update converter.vhd

--- Labs/Lab9/converter.vhd
+++ Labs/Lab9/converter.vhd
@@ -4,123 +4,115 @@ use IEEE.numeric_std.all;
 
 entity Converter is    
 port(
-    clk     :   IN std_ulogic;    
-    i       :   IN  signed(10 downto 0);          -- 0 to 1023 plus sign bit
-    seg0    :   OUT std_ulogic_vector(7 downto 0);
-    seg1    :   OUT std_ulogic_vector(7 downto 0);
-    seg2    :   OUT std_ulogic_vector(7 downto 0);
-    seg3    :   OUT std_ulogic_vector(7 downto 0)
+    clk     :   IN  std_ulogic;    
+    number  :   IN  unsigned(9 downto 0);          -- from '0' up to '999'
+    sign    :   IN  std_logic,
+    seg     :   OUT std_ulogic_vector(31 downto 0);
 );
 end entity;
 
 architecture behave of Converter is
-    type    cState      is  (Rec, Mult, Add, Output);
-    signal  state       :   cState                  := Rec;
-    signal  n0, n1, n2  :   unsigned(3 downto 0)    := x"0";
-    signal  c0, c1, c2, c3  :   std_ulogic          := '0';
-    signal  s           :   unsigned(3 downto 0)    := x"F";
-    signal  reg         :   unsigned(9 downto 0)    := x"0";
+    -- states
+    type    cState  is  (Idle, Convert);
+    signal  state   :   cState  :=  Idle;
+    -- shift register and array of decimal digits
+    signal  reg     :   unsigned(9 downto 0)    := x"0";
+    signal  num     :   array(2 downto 0) of unsigned(3 downto 0);
+    -- array of ouput decimal digits
+    -- -- TODO: add an array, implement 'Output' state
+    -- carry
+    signal  carry   :   std_logic;
+    -- sign output
+    signal  num_sign:   unsigned(3 downto 0)    := x"F"; -- plus  
+    -- counters
+    signal  counter :   unsigned(3 downto 0);
+    signal  digit   :   unsigned(1 downto 0);
 begin
+    
+    -- sign ouput
+    if sign = '1' then
+        num_sign <= x"A";
+    else
+        num_sign <= x"F";
+    end if;
+
     process(clk)
     begin
         if rising_edge(clk) then
             case state is
-                when Rec  =>
-                    reg <=  i(9 downto 0);
-                    n0  <=  x"0";
-                    n1  <=  x"0";
-                    n2  <=  x"0";
-                    c0  <=  '0';
-                    c1  <=  '0';
-                    c2  <=  '0';
-                    c3  <=  '0';
-                    
-                    if reg(10) = '1' then
-                        s   <=  x"A";   -- minus
+                when Idle  =>
+                    -- start loop for nonzero input values
+                    if number > 999 then
+                        num(0)  <=  x"A";   -- set all digits to 'minus sign'
+                        num(1)  <=  x"A";
+                        num(2)  <=  x"A";
                     else
-                        s   <=  x"F";   -- plus
-                    end if;                    
-                    state   <=  Mult;
-                    
-                when Mult =>
-                    if reg = 0 then
-                        state   <=  Output;
-                    else
-                        c0      <=  reg(9);
-                        reg(9)  <=  reg(8 downto 0) & '0';  -- shifting
-                        
-                        if n0 > 4 and n0 < 10 then
-                            c1  <=  '1';    -- carry
-                            n0  <=  (n0 - 5) * 2;
-                        else
-                            c1  <=  '0';
-                            n0  <=  n0 * 2;
-                        end if;                        
-                        
-                        if n1 > 4 and n1 < 10 then
-                            c2  <=  '1';    -- carry
-                            n1  <=  (n1 - 5) * 2;
-                        else
-                            c2  <=  '0';
-                            n1  <=  n1 * 2;
-                        end if;                        
-                        
-                        if n2 > 4 and n2 < 10 then
-                            c3  <=  '1';    -- carry
-                            n2  <=  (n2 - 5) * 2;
-                        else
-                            c3  <=  '0';
-                            n2  <=  n2 * 2;
-                        end if;
-                        
-                        state   <=  Add;
+                        -- set decimal digits to zero
+                        num(0)  <=  x"0";
+                        num(1)  <=  x"0";
+                        num(2)  <=  x"0";
+                        -- buffer
+                        reg     <=  i(9 downto 0);
+                        -- carry
+                        carry   <=  i(9);         
+                        -- initialize shift-counter and digit-counter
+                        counter <= 0;
+                        digit   <= 0;
+                        -- switch to loop state
+                        state <=  Convert;
                     end if;
                     
-                when Add    =>                
-                    if c3 = '1' then    -- incorrect output, number is too big
-                        n0  <= x"A";      -- minus sign
-                        n1  <= x"A";      -- minus sign
-                        n2  <= x"A";      -- minus sign
-                        s   <= x"A";      -- minus sign
-                        state   <=  Rec;
+                when Convert =>
+                    if counter <= reg'high then
+                        -- switch between digits
+                        if digit <= 2 then                            
+                            if num(to_integer(digit)) >= 5 and num(to_integer(digit)) <= 9 then
+                                num(to_integer(digit))  <=  (num(to_integer(digit)) - 5) * 2 + carry;
+                                carry   <=  '1';
+                            else
+                                num(to_integer(digit))  <=  num(to_integer(digit)) * 2 + carry;
+                                carry   <=  '0';
+                            end if;  
+                            digit   <=  digit + 1;
+                        else    
+                            -- get the carry
+                            carry   <=  i(8);
+                            -- shift the register
+                            reg     <=  reg(8 downto 0) & '0';
+                            -- increment shift-counter
+                            counter <=  counter + 1;
+                            -- set digit-counter to zero
+                            digit   <=  0;
+                        end if;
                     else
-                        if c0 = '1' then
-                            n0  <= n0 + 1;
-                        end if;
-                        if c1 = '1' then
-                            n1  <= n1 + 1;
-                        end if;
-                        if c2 = '1' then
-                            n2  <= n2 + 1;
-                        end if;
-                        state   <=  Mult;
-                    end if;
+                        state   <=  Idle;
+                    end if;                
             end case;
         end if;
     end process;
     
     seg_0   :   entity work.Segment
     port map(
-        i   =>  n0,
-        o   =>  seg0
+        i   =>  num(0),
+        o   =>  seg(7 downto 0)
     );
     
     seg_1   :   entity work.Segment
     port map(
-        i   =>  n1,
-        o   =>  seg1
+        i   =>  num(1),
+        o   =>  seg(15 downto 8)
     );
     
     seg_2   :   entity work.Segment
     port map(
-        i   =>  n2,
-        o   =>  seg2
+        i   =>  num(2),
+        o   =>  seg(23 downto 16)
     );
     
     seg_sign:   entity work.Segment
     port map(
-        i   =>  s,
-        o   =>  seg3
+        i   =>  num_sign,
+        o   =>  seg(31 downto 24)
     );
     
 end architecture;
